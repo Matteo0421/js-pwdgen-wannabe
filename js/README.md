bcc938bfcac3406fb1b174f31ef56ab6506152fb
Matteo0421 <matteo.carola99@gmail.com>
2024-02-19 15:41:10 +0100

    [fix]exercise

--- js/README.md
+++ js/README.md
@@ -11,4 +11,6 @@ Infine scrivi sulla pagina nomecognomecolorepreferito24
  1. con un prompt chiedere il nome  all'utente e salvarla in una variabile
  2. con un prompt chiedere il cognome  all'utente e salvarla in una variabile  
  3. con un prompt chiedere il colore preferito  all'utente e salvarla in una variabile  
- 4. selezionare l'elemento dove stampare l'output con innerHTML
+ 4. con un prompt chiedere il numero preferito  all'utente e salvarla in una variabile  
+ 5. creare una variabile con tutti i dati inseriti dall'utente
+ 6. selezionare l'elemento dove stampare l'output con innerHTML
--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 //1
 const firstName = prompt('Inserisci il nome')
 
+//2
 const lastName = prompt('Inserisci il cognome')
 
+//3
 const favouriteColor = prompt('Inserisci il tuo colore preferito')
 
-console.log (firstName, lastName, favouriteColor);
+//4
+const favouriteNumber = prompt('Inserisci il tuo numero preferito')
+
+console.log (firstName, lastName, favouriteColor, favouriteNumber);
+
+//5
+const password = firstName + lastName + favouriteColor + favouriteNumber
 
 document.getElementById('prova').innerHTML = 
 `
 <p>
-Il tuo nome è ${firstName} ${lastName}, il tuo colore preferitpo è il ${favouriteColor}
+${password}
 </p>
 `
